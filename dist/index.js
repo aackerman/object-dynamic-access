dd735b1ab292840b29f28131889169ea74a846f2
Aaron Ackerman <theron17@gmail.com>
2015-01-18 17:06:17 -0600

    Export to CommonJS only by default

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,14 @@
-(function (factory) {
-  if (typeof define === "function" && define.amd) {
-    define(["exports"], factory);
-  } else if (typeof exports !== "undefined") {
-    factory(exports);
-  }
-})(function (exports) {
-  "use strict";
+"use strict";
 
-  exports["default"] = get;
-  function get(obj, path) {
-    if (typeof path == "string") path = path.split(".");
-    if (obj === undefined) return;
-    if (path.length === 0) return obj;
-    if (obj === null) return;
-    var next = path[0];
-    if (next.match(/\[(.+)\]/)) {
-      next = RegExp.$1;
-    }
-    return get(obj[next], path.slice(1));
+module.exports = get;
+function get(obj, path) {
+  if (typeof path == "string") path = path.split(".");
+  if (obj === undefined) return;
+  if (path.length === 0) return obj;
+  if (obj === null) return;
+  var next = path[0];
+  if (next.match(/\[(.+)\]/)) {
+    next = RegExp.$1;
   }
-});
+  return get(obj[next], path.slice(1));
+}
